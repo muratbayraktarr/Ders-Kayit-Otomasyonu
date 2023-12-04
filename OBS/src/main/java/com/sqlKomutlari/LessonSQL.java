af55e54753b12d2f58d74d08a74b689a30ca30a9
Murat Bayraktar <210205006@ostimteknik.edu.tr>
2023-12-05 00:49:11 +0300

    LessonSQL Düzenlendi

--- OBS/src/main/java/com/sqlKomutlari/LessonSQL.java
+++ OBS/src/main/java/com/sqlKomutlari/LessonSQL.java
@@ -1,4 +1,3 @@
-
 package com.sqlKomutlari;
 
 import com.db.MySQLVeritabaniBaglantisi;
@@ -14,7 +13,8 @@ import java.util.ArrayList;
  * @author ByMazarat
  */
 public class LessonSQL {
-    public String LessonAdd(String name, int max_capacity, int department_id, int instructor_id, String day, String hours, int level,String classroom) {
+
+    public String LessonAdd(String name, int max_capacity, int department_id, int instructor_id, String day, String hours, int level, String classroom) {
         MySQLVeritabaniBaglantisi db = new MySQLVeritabaniBaglantisi();
         String Message = "";
         Connection conn = null;
@@ -22,49 +22,55 @@ public class LessonSQL {
             conn = db.baglantiyiAl();
             if (conn != null) {
                 System.out.println("Başarılı");
+                if (IsClassFree(day, hours, classroom, conn)) {
+                    String insertSQL = "INSERT INTO courses (course_name,max_capacity,department_id,instructor_id,day,hours,level,class) VALUES (?,?,?,?,?,?,?,?)";
 
-                String insertSQL = "INSERT INTO courses (course_name,max_capacity,department_id,instructor_id,day,hours,level,class) VALUES (?,?,?,?,?,?,?,?)";
+                    PreparedStatement statement = conn.prepareStatement(insertSQL);
+                    statement.setString(1, name);
+                    statement.setInt(2, max_capacity);
+                    statement.setInt(3, department_id);
+                    statement.setInt(4, instructor_id);
+                    statement.setString(5, day);
+                    statement.setString(6, hours);
+                    statement.setInt(7, level);
+                    statement.setString(8, classroom);
 
-                PreparedStatement statement = conn.prepareStatement(insertSQL);
-                statement.setString(1, name);
-                statement.setInt(2, max_capacity);
-                statement.setInt(3, department_id);
-                statement.setInt(4, instructor_id);
-                statement.setString(5, day);
-                statement.setString(6, hours);
-                statement.setInt(7, level);
-                statement.setString(8, classroom);
+                    int rowsInserted = 0;
 
-                int rowsInserted = 0;
+                    try {
+                        rowsInserted = statement.executeUpdate();
+                        System.out.println(rowsInserted);
+                    } catch (SQLException e) {
+                        rowsInserted = 0;
+                        e.printStackTrace();
+                    }
 
-                try {
-                    rowsInserted = statement.executeUpdate();
-                    System.out.println(rowsInserted);
-                } catch (SQLException e) {
-                    rowsInserted = 0;
-                    e.printStackTrace();
+                    if (rowsInserted > 0) {
+                        Message = "Veri Başarıyla Kaydedildi.";
+
+                    } else {
+                        Message = "Kaydetme işlemi başarısız.";
+                    }
+                }
+                else{
+                    Message = "Belirttiğiniz tarih ve saatte bu sınıf dolu.";
                 }
 
-                if (rowsInserted > 0) {
-                    Message = "Veri Başarıyla Kaydedildi.";
-                    
-                } else {
-                    Message = "Kaydetme işlemi başarısız.";
-                }
             } else {
                 Message = "Bağlantı hatası: Bağlantı null veya kapalı.";
-                
+
             }
         } catch (Exception e) {
             e.printStackTrace();
             Message = "Veritabanına kaydetme sırasında bir hata oluştu.";
-            
+
         } finally {
             conn = db.baglantiyiKapat();
             return Message;
         }
     }
-    public ArrayList<Lesson> getLesson(){
+
+    public ArrayList<Lesson> getLesson() {
         ArrayList<Lesson> veriListesi = new ArrayList<Lesson>();
         MySQLVeritabaniBaglantisi veritabaniBaglantisi = new MySQLVeritabaniBaglantisi();
         Connection conn = null;
@@ -85,9 +91,9 @@ public class LessonSQL {
                     int capacity = resultSet.getInt("capacity");
                     int max_capacity = resultSet.getInt("max_capacity");
                     int department_id = resultSet.getInt("department_id");
-                    int instructor_id = resultSet.getInt("instructor_id");                    
+                    int instructor_id = resultSet.getInt("instructor_id");
                     String day = resultSet.getString("day");
-                    String hours = resultSet.getString("hours");                    
+                    String hours = resultSet.getString("hours");
                     int level = resultSet.getInt("level");
                     String classroom = resultSet.getString("class");
                     String department = null;
@@ -96,9 +102,9 @@ public class LessonSQL {
                     statement = conn.prepareStatement(sql);
                     statement.setInt(1, instructor_id);
                     resultSet2 = statement.executeQuery();
-                    while(resultSet2.next()){
+                    while (resultSet2.next()) {
                         try {
-                            instructor = resultSet2.getString("name");                            
+                            instructor = resultSet2.getString("name");
                         } catch (Exception e) {
                             e.printStackTrace();
                         }
@@ -107,14 +113,13 @@ public class LessonSQL {
                     statement = conn.prepareStatement(sql);
                     statement.setInt(1, department_id);
                     resultSet2 = statement.executeQuery();
-                    while(resultSet2.next()){
+                    while (resultSet2.next()) {
                         try {
-                            department = resultSet2.getString("department_name");                            
+                            department = resultSet2.getString("department_name");
                         } catch (Exception e) {
                             e.printStackTrace();
                         }
                     }
-                    System.out.println("Lessondaki veriler kaydedildi");
                     Lesson lesson = new Lesson(id, name, capacity, max_capacity, department_id, department, instructor_id, instructor, day, hours, level, classroom);
                     veriListesi.add(lesson);
                 } catch (Exception e) {
@@ -136,7 +141,7 @@ public class LessonSQL {
                 if (resultSet2 != null) {
                     resultSet2.close();
                 }
-                veritabaniBaglantisi.baglantiyiKapat();
+                conn = veritabaniBaglantisi.baglantiyiKapat();
             } catch (SQLException e) {
                 e.printStackTrace();
                 System.out.println("Bağlantı kapatılamadı");
@@ -144,11 +149,61 @@ public class LessonSQL {
         }
         return veriListesi;
     }
+
+    public boolean IsClassFree(String checkDay, String checkHours, String checkClassroom, Connection conn) {
+        ArrayList<Lesson> veriListesi = new ArrayList<Lesson>();
+        MySQLVeritabaniBaglantisi veritabaniBaglantisi = new MySQLVeritabaniBaglantisi();
+        PreparedStatement statement = null;
+        ResultSet resultSet = null;
+        boolean value = true;
+        try {    
+            String sql = "SELECT * FROM courses";
+            statement = conn.prepareStatement(sql);
+            resultSet = statement.executeQuery();
+
+            while (resultSet.next()) {
+                try {
+
+                    String day = resultSet.getString("day");
+                    String hours = resultSet.getString("hours");
+                    String classroom = resultSet.getString("class");
+                    if (day.equals(checkDay) && hours.equals(checkHours) && classroom.equals(checkClassroom)) {
+                        value = false;
+                        break;
+                    }
+
+                } catch (Exception e) {
+                    e.printStackTrace();
+                }
+
+            }
+        } catch (SQLException e) {
+            e.printStackTrace();
+            System.out.println("SQL hatası oluştu");
+            conn = veritabaniBaglantisi.baglantiyiKapat();
+
+        } finally {
+            try {
+                if (resultSet != null) {
+                    resultSet.close();
+                }
+                if (statement != null) {
+                    statement.close();
+                }
+
+            } catch (SQLException e) {
+                e.printStackTrace();
+                System.out.println("Bağlantı kapatılamadı");
+            }
+        }
+        return value;
+
+    }
+
     public static void main(String[] args) {
-        LessonSQL veriCek = new LessonSQL(); 
+        LessonSQL veriCek = new LessonSQL();
         ArrayList<Lesson> LessonList = veriCek.getLesson();
-        for(Lesson lesson : LessonList)
-        {
+        for (Lesson lesson : LessonList) {
             System.out.println(lesson.getName());
         }
     }
